2ba76c7d56e80ce7620bac39be5983d3af3f9276
Gabriel447 <fullstackgabe@gmail.com>
2025-05-03 18:27:32 -0300

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,45 +4,34 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card main-card">
+            <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <h3 class="text-center mb-5 welcome-text">Bem-vindo {{ Auth::user()->name }}!</h3>
-
-                    <div class="row text-center">
-                        <div class="col-md-4 mb-3">
-                            <a href="{{ route('movies.index') }}" class="text-decoration-none">
-                                <div class="menu-button">
-                                    <p class="fs-4 mb-0">Filmes</p>
-                                </div>
+                    <h2 class="text-center mb-4">Bem-vindo Gabe!</h2>
+                    
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <a href="{{ route('movies.index') }}" class="btn btn-primary w-100">
+                                Filmes
                             </a>
                         </div>
-                        <div class="col-md-4 mb-3">
-                            <a href="{{ route('clientes.index') }}" class="text-decoration-none">
-                                <div class="menu-button">
-                                    <p class="fs-4 mb-0">Clientes</p>
-                                </div>
+                        <div class="col-md-6">
+                            <a href="{{ route('clientes.index') }}" class="btn btn-primary w-100">
+                                Clientes
                             </a>
                         </div>
-                        <div class="col-md-4 mb-3">
-                            <a href="{{ route('locacoes.index') }}" class="text-decoration-none">
-                                <div class="menu-button">
-                                    <p class="fs-4 mb-0">Locações</p>
-                                </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="{{ route('locacoes.index') }}" class="btn btn-primary w-100">
+                                Locações
                             </a>
                         </div>
-                        <div class="col-md-4 mb-3">
-                            <a href="{{ route('devolucoes.index') }}" class="text-decoration-none">
-                                <div class="menu-button">
-                                    <p class="fs-4 mb-0">Devoluções</p>
-                                </div>
+                        <div class="col-md-6">
+                            <a href="{{ route('devolucoes.index') }}" class="btn btn-primary w-100">
+                                Devoluções
                             </a>
                         </div>
                     </div>
