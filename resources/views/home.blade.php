47fe901b1989f126302d00a92deb8ec0b9d19e2b
Gabriel447 <fullstackgabe@gmail.com>
2025-05-07 22:49:56 -0300

    tela principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,33 +30,33 @@ h2 {
 
 <div class="container py-4">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body mb-2">
                     <h2 class="text-center mb-4">Bem-vindo {{ Auth::user()->name }}!</h2>
                     
-                    <div class="row mb-3">
-                        <div class="col-md-6 px-2">
+                    <div class="row mb-3 justify-content-center">
+                        <div class="col-md-3 px-2">
                             <a href="{{ route('movies.index') }}" class="btn btn-primary w-100">
                                 Filmes
                             </a>
                         </div>
-                        <div class="col-md-6 px-2">
+                        <div class="col-md-3 px-2">
                             <a href="{{ route('clientes.index') }}" class="btn btn-primary w-100">
                                 Clientes
                             </a>
                         </div>
                     </div>
                     
-                    <div class="row">
-                        <div class="col-md-6 px-2">
+                    <div class="row justify-content-center">
+                        <div class="col-md-3 px-2">
                             <a href="{{ route('locacoes.index') }}" class="btn btn-primary w-100">
                                 Locações
                             </a>
                         </div>
-                        <div class="col-md-6 px-2">
+                        <div class="col-md-3 px-2">
                             <a href="{{ route('devolucoes.index') }}" class="btn btn-primary w-100">
                                 Devoluções
                             </a>
